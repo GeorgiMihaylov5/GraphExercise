f9f844a19d4188ea3174ae096ad0e4b999514325
Georgi <Georgi@DESKTOP-8EDNSH6>
2023-01-27 23:16:59 +0200

    Add project files.

---
+++ Troiki/DepthFirstSearch.cs
@@ -0,0 +1,84 @@
+﻿namespace Troiki
+{
+    public class DepthFirstSearch
+    {
+        //We use IEnumerable because it's a good practice in our opinion.
+        private IEnumerable<int[]> matrix;
+        private bool[] used;
+        private int n;
+        private int target;
+        private bool isConflict;
+
+        private void DepthSearch(int node)
+        {
+            //Bottom of recursion (дъно на рекурсията).
+            if (node == target)
+            {
+                if (isConflict)
+                {
+                    isConflict = false;
+                }
+                return;
+            }
+
+            used[node] = true;
+
+            //Iterates through the matrix and goes to the next node.
+            //Use recursion and the method accepts the next node.
+            for (int i = 0; i < n; i++)
+            {
+                if (!used[i] && matrix.ElementAt(node)[i] > 0)
+                {
+                    DepthSearch(i);
+                }
+            }
+
+            used[node] = false;
+        }
+
+        public bool FindConflict(IEnumerable<int[]> matrix, int cities, int startNode, int searchedNode)
+        {
+            //We add 1 to exclude the 0 index.
+            n = cities + 1;
+            this.matrix = GetEdges(matrix);
+
+            target = searchedNode;
+            isConflict = true;
+
+            //Saves information whether the node is used
+            used = new bool[n];
+
+            DepthSearch(startNode);
+
+            return isConflict;
+        }
+
+        public void AddEdge(IEnumerable<int[]> matrix, int a, int b, int weight = 1)
+        {
+            matrix.ElementAt(a)[b] = weight;
+            matrix.ElementAt(b)[a] = weight;
+        }
+
+        //Returns an array with information about edges in the graph.
+        //Because the array is two dimensional, we use two indexes to check for edges between two nodes.
+        //If the value of the indexes is 0 they don't have an edge, otherwise they do.
+        private IEnumerable<int[]> GetEdges(IEnumerable<int[]> matrix)
+        {
+            var list = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                list[i] = new int[n];
+            }
+
+            foreach (var edge in matrix)
+            {
+                AddEdge(list, edge.ElementAt(0), edge.ElementAt(1));
+                AddEdge(list, edge.ElementAt(0), edge.ElementAt(2));
+                AddEdge(list, edge.ElementAt(1), edge.ElementAt(2));
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Troiki/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Troiki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse).ToArray();
+
+            var cities = input[0];
+            var nodes = input[1];
+
+            //Creating jagged array.
+            var arr = new int[nodes][];
+
+            //Fills the array with elements.
+            for (int i = 0; i < nodes; i++)
+            {
+                arr[i] = Console.ReadLine()
+                .Split()
+                .Select(int.Parse).ToArray();
+            }
+
+            //We use Depth First Search, because we think this is the better option for our project.
+            var dfs = new DepthFirstSearch();
+            //We use HashSet to remove dublicated conflict nodes.
+            var conflicts = new HashSet<int>();
+
+            //This for loop iterates the nodes(възлите)
+            for (int i = 0; i < nodes; i++)
+            {
+                //Except the one node
+                var currentArr = arr.Where(x => x != arr[i]).ToArray();
+
+
+                //iterates throuth the cities from the current city, searching for the last city
+                for (int j = 1; j < cities; j++)
+                {
+                    var isConflict = dfs.FindConflict(currentArr, cities, j, cities);
+
+                    //Checking for conflicts and adding them to the hashset.
+                    if (isConflict)
+                    {
+                       conflicts.Add(i + 1); 
+                    }
+                }
+            }
+
+            Console.WriteLine(conflicts.Count);
+            Console.WriteLine(string.Join(" ", conflicts));
+        }
+    }
+}
